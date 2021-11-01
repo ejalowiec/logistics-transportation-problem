ecdde579b46ce44f976d8a08e158a7515fa8d2b0
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2021-11-01 20:02:17 +0100

    added aggregator class

---
+++ src/com/transportationproblem/Aggregator.java
@@ -0,0 +1,84 @@
+package com.transportationproblem;
+
+public class Aggregator {
+    private int D1supply;
+    private int D1buyingPrice;
+    private int D2supply;
+    private int D2buyingPrice;
+    private int O1demand;
+    private int O1sellingPrice;
+    private int O2demand;
+    private int O2sellingPrice;
+    private int O3demand;
+    private int O3sellingPrice;
+    private int D1O1transportationCost;
+    private int D1O2transportationCost;
+    private int D1O3transportationCost;
+    private int D2O1transportationCost;
+    private int D2O2transportationCost;
+    private int D2O3transportationCost;
+
+    public int getD1supply() {
+        return D1supply;
+    }
+
+    public int getD1buyingPrice() {
+        return D1buyingPrice;
+    }
+
+    public int getD2supply() {
+        return D2supply;
+    }
+
+    public int getD2buyingPrice() {
+        return D2buyingPrice;
+    }
+
+    public int getO1demand() {
+        return O1demand;
+    }
+
+    public int getO1sellingPrice() {
+        return O1sellingPrice;
+    }
+
+    public int getO2demand() {
+        return O2demand;
+    }
+
+    public int getO2sellingPrice() {
+        return O2sellingPrice;
+    }
+
+    public int getO3demand() {
+        return O3demand;
+    }
+
+    public int getO3sellingPrice() {
+        return O3sellingPrice;
+    }
+
+    public int getD1O1transportationCost() {
+        return D1O1transportationCost;
+    }
+
+    public int getD1O2transportationCost() {
+        return D1O2transportationCost;
+    }
+
+    public int getD1O3transportationCost() {
+        return D1O3transportationCost;
+    }
+
+    public int getD2O1transportationCost() {
+        return D2O1transportationCost;
+    }
+
+    public int getD2O2transportationCost() {
+        return D2O2transportationCost;
+    }
+
+    public int getD2O3transportationCost() {
+        return D2O3transportationCost;
+    }
+}
