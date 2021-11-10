5f4cb1d88f9a0d3445c02c7b6b12901aeb0add17
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2021-11-10 08:17:47 +0100

    added displaying all of the values in DisplayResult class

--- src/com/transportationproblem/DisplayResult.java
+++ src/com/transportationproblem/DisplayResult.java
@@ -7,37 +7,39 @@ import java.awt.event.ActionListener;
 
 public class DisplayResult {
 
+    public static final int WIDTH = 600;
+
     public DisplayResult() {
 
         // init frame
 
         JFrame frame = new JFrame("Transportation problem");
-        frame.setSize(1250, 1000);
+        frame.setSize(WIDTH + 60, 900);
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
         JLabel unitProfitMatrix = new JLabel();
-        unitProfitMatrix.setBounds(20,20,1200,30);
+        unitProfitMatrix.setBounds(20,20,WIDTH,30);
 
         JTextArea unitProfitArea = new JTextArea();
-        unitProfitArea.setBounds(20, 50, 1200, 75);
+        unitProfitArea.setBounds(20, 50, WIDTH, 150);
 
         JLabel optimalTransport = new JLabel();
-        optimalTransport.setBounds(20,130,1200,30);
+        optimalTransport.setBounds(20,280,WIDTH,30);
 
         JTextArea optimalTransportArea = new JTextArea();
-        optimalTransportArea.setBounds(20, 160, 1200, 100);
+        optimalTransportArea.setBounds(20, 320, WIDTH, 200);
 
         JLabel kosztCalk = new JLabel();
-        kosztCalk.setBounds(20,280,1200,30);
+        kosztCalk.setBounds(20,600,WIDTH,30);
 
         JLabel przychodCalk = new JLabel();
-        przychodCalk.setBounds(20,320,1200,30);
+        przychodCalk.setBounds(20,650,WIDTH,30);
 
         JLabel zyskPosr = new JLabel();
-        zyskPosr.setBounds(20,360,1200,30);
+        zyskPosr.setBounds(20,700,WIDTH,30);
 
 
         // display results
@@ -46,10 +48,15 @@ public class DisplayResult {
         unitProfitArea.setText(Logic.getMacierzZyskowJednostkowych());
         unitProfitArea.setFont(new Font("Calibri", Font.BOLD, 24));
         optimalTransport.setText("Tabela optymalnych przewozów:");
+        optimalTransport.setFont(new Font("Calibri", Font.BOLD, 24));
         optimalTransportArea.setText(Logic.getmacierzOptymalnychPrzewozow());
+        optimalTransportArea.setFont(new Font("Calibri", Font.BOLD, 24));
         kosztCalk.setText("Koszt całkowity: " + Double.toString(Logic.getKosztCalk()));
+        kosztCalk.setFont(new Font("Calibri", Font.BOLD, 24));
         przychodCalk.setText("Przychód całkowity: " + Double.toString(Logic.getPrzychodCalk()));
+        przychodCalk.setFont(new Font("Calibri", Font.BOLD, 24));
         zyskPosr.setText("Zysk pośrednika: " + Double.toString(Logic.getZyskPosr()));
+        zyskPosr.setFont(new Font("Calibri", Font.BOLD, 24));
 
         frame.add(unitProfitMatrix);
         frame.add(unitProfitArea);
